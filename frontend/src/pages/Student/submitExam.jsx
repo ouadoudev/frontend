50d14e483b538acd66e3bd11cfc473f4e8b355f5
OUADOU MOHAMED <158632869+ouadoudev@users.noreply.github.com>
2025-10-09 04:16:25 +0100

    Update submitExam.jsx

--- frontend/src/pages/Student/submitExam.jsx
+++ frontend/src/pages/Student/submitExam.jsx
@@ -1054,6 +1054,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import katex from "katex";
+import "katex/dist/katex.min.css";
 
 // RTL Detection Function
 const getDirection = (text) => {
